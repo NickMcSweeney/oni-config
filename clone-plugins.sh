8e2af56b5ec173a508d1d63457cbd4b6bbe8ad2b
Nick <Shindler25@gmail.com>
2019-06-03 23:47:56 +0200

    Create clone-plugins.sh

---
+++ clone-plugins.sh
@@ -0,0 +1,12 @@
+cd plugins &&
+\ git clone https://github.com/w0rp/ale.git &&
+\ git clone https://github.com/morhetz/gruvbox.git &&
+\ git clone https://github.com/ervandew/supertab.git &&
+\ git clone https://github.com/othree/javascript-libraries-syntax.vim.git &&
+\ git clone https://github.com/vim-scripts/SyntaxComplete.git &&
+\ git clone https://github.com/SirVer/ultisnips.git &&
+\ git clone https://github.com/pangloss/vim-javascript.git &&
+\ git clone https://github.com/honza/vim-snippets.git &&
+\ git clone https://github.com/iloginow/vim-stylus.git &&
+\ git clone https://github.com/posva/vim-vue.git &&
+\ git clone https://github.com/Valloric/YouCompleteMe.git
