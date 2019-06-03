3a189e96e744edf55f7c99cbf61010819f623800
Nick <Shindler25@gmail.com>
2019-06-03 23:58:10 +0200

    Update clone-plugins.sh

--- clone-plugins.sh
+++ clone-plugins.sh
@@ -1,12 +1,20 @@
-cd plugins &&
-\ git clone https://github.com/w0rp/ale.git &&
-\ git clone https://github.com/morhetz/gruvbox.git &&
-\ git clone https://github.com/ervandew/supertab.git &&
-\ git clone https://github.com/othree/javascript-libraries-syntax.vim.git &&
-\ git clone https://github.com/vim-scripts/SyntaxComplete.git &&
-\ git clone https://github.com/SirVer/ultisnips.git &&
-\ git clone https://github.com/pangloss/vim-javascript.git &&
-\ git clone https://github.com/honza/vim-snippets.git &&
-\ git clone https://github.com/iloginow/vim-stylus.git &&
-\ git clone https://github.com/posva/vim-vue.git &&
-\ git clone https://github.com/Valloric/YouCompleteMe.git
+#!/bin/sh
+
+# all my plugins
+cloneifexist() {
+	git clone https://github.com/w0rp/ale.git;
+	git clone https://github.com/morhetz/gruvbox.git;
+	git clone https://github.com/ervandew/supertab.git;
+	git clone https://github.com/othree/javascript-libraries-syntax.vim.git;
+	git clone https://github.com/vim-scripts/SyntaxComplete.git;
+	git clone https://github.com/SirVer/ultisnips.git;
+	git clone https://github.com/pangloss/vim-javascript.git;
+	git clone https://github.com/honza/vim-snippets.git;
+	git clone https://github.com/iloginow/vim-stylus.git;
+	git clone https://github.com/posva/vim-vue.git;
+	git clone https://github.com/Valloric/YouCompleteMe.git;
+};
+
+# clone plugins
+echo "Cloning plugins from git repos"
+cd plugins && cloneifexist
